d19b253f283abb5a6fc55e0eb617ca63bd18b008
Rishabh <rishabhgupta7199@gmail.com>
2022-09-16 11:50:01 +0530

    added the connect method and print

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -1,7 +1,18 @@
 package com.database;
 
+import com.database.service.db.DatabaseService;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseService db = new DatabaseService();
+        try {
+            db.connect();
+            db.printMessage();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/java/com/database/service/db/DatabaseService.java
+++ src/main/java/com/database/service/db/DatabaseService.java
@@ -1,7 +1,33 @@
 package com.database.service.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseService {
-    public boolean connect() {
-        return false;
+    public static final String URL = "jdbc:mysql://127.0.0.1:3307/movies";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+    private Connection databaseConnection;
+
+    public DatabaseService() {
+        this.databaseConnection = null;
+    }
+
+    public boolean connect() throws ClassNotFoundException, SQLException {
+        // Load the jdbc driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //Create
+        databaseConnection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return databaseConnection != null;
+    }
+
+    public void printMessage() {
+        if (databaseConnection != null) {
+            System.out.println("Connected to the Database");
+        } else {
+            System.err.println("Not connected to the database");
+        }
     }
 }
